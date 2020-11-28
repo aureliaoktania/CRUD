fb128ea6ddb7c51bedc2dd73cc55e5cdc0b57438
aureliaoktania <aurelia.oktania@gmail.com>
2020-11-28 22:04:15 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,50 +1,71 @@
 <html>
 <head>
-<title>Belajar Bootstrap</title>
+<title>Toys Town</title>
 <link href="./css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
+<h1>Filter</h1>
+<button type="button" class="btn btn-primary float-right">Add New</button> <br>
  <div class="row">
-  <div class="col-md-4">
-   <h1>Filter</h1>
-   Nama
+  <div>
+   Name
    <input type="text" name="nama" class="form-control" value=""/><hr>
-   <input type="submit" name="filter" class="btn btn-primary" value="Filter"/>
-  </div>
+
+   Code
+   <input type="text" name="nama" class="form-control" value=""/><hr> <br>
+</div>
  </div>
+
+ <input type="submit" name="filter" class="btn btn-primary" value="Search"/>
+ <input type="submit" name="filter" class="btn btn-secondary" value="Reset"/> <br> <br>
+
  <div class="row">
   <div class="col-md-12">
    <table width="100%" class="table table-bordered table-striped">
     <tr>
-     <th>No</th>
-     <th>NIP</th>
-     <th>Nama</th>
-     <th>Alamat</th>
+     <th>Name</th>
+     <th>Code</th>
+     <th>Category</th>
+     <th>Price</th>
+     <th>Stock Account</th>
+     <th>Action</th>
     </tr>
     <tr>
      <td>1</td>
      <td>100120</td>
      <td>Orga</td>
      <td>Jakarta Barat No 58</td>
+     <td>50</td>
+     <td><button type="button" class="btn btn-success btn-sm">Edit</button>
+      <button type="button" class="btn btn-danger btn-sm">Delete</button></td>
     </tr>
     <tr>
      <td>2</td>
      <td>100121</td>
      <td>Mikazuki</td>
      <td>Depok</td>
+     <td>50</td>
+     <td><button type="button" class="btn btn-success btn-sm">Edit</button>
+      <button type="button" class="btn btn-danger btn-sm">Delete</button></td>
     </tr>
     <tr>
      <td>3</td>
      <td>100122</td>
      <td>Akihiro</td>
      <td>Jakarta Pusat No 100</td>
+     <td>50</td>
+     <td><button type="button" class="btn btn-success btn-sm">Edit</button>
+      <button type="button" class="btn btn-danger btn-sm">Delete</button></td>
     </tr>
     <tr>
      <td>4</td>
      <td>100123</td>
      <td>Lia</td>
      <td>Jakarta Selatan,Lebak Bulus</td>
+     <td>50</td>
+     <td><button type="button" class="btn btn-success btn-sm">Edit</button>
+      <button type="button" class="btn btn-danger btn-sm">Delete</button></td>
     </tr>
    </table>
   </div>

4ca45050b766c12582cf4cc837eb8178f26300fa
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-28 22:07:13 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,6 +4,31 @@
 <link href="./css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="">♛ Toys Town</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+ 
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="lu tambahin ae mau kemane">Dasboard<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="jee mau kemane ">Stock</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      
+    <div class="jumbotron">
+        <h1 class="display-4">Manage Inventory</h1>
+        <p class="lead">Set and manage your store inventory here.</p>
+        <hr class="my-4">
+        <p>Click the <strong>Add New</strong> to insert new inventory</p><br>
+        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#newprod">Add New</button> 
+    </div>
 <div class="container">
 <h1>Filter</h1>
 <button type="button" class="btn btn-primary float-right">Add New</button> <br>

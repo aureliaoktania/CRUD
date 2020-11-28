d03e81827627b9e89f3458f79a05e4886d7f40f4
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-28 21:36:31 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,26 +1,55 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-    <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="./css/bootstrap.min.css">
- 
-    <title>Semangatt</title>
-  </head>
-  <body>
-  
-  
-  <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+<html>
+<head>
+<title>Belajar Bootstrap</title>
+<link href="template/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+ <div class="row">
+  <div class="col-md-4">
+   <h1>Filter</h1>
+   Nama
+   <input type="text" name="nama" class="form-control" value=""/><hr>
+   <input type="submit" name="filter" class="btn btn-primary" value="Filter"/>
+  </div>
+ </div>
+ <div class="row">
+  <div class="col-md-12">
+   <table width="100%" class="table table-bordered table-striped">
+    <tr>
+     <th>No</th>
+     <th>NIP</th>
+     <th>Nama</th>
+     <th>Alamat</th>
+    </tr>
+    <tr>
+     <td>1</td>
+     <td>100120</td>
+     <td>Orga</td>
+     <td>Jakarta Barat No 58</td>
+    </tr>
+    <tr>
+     <td>2</td>
+     <td>100121</td>
+     <td>Mikazuki</td>
+     <td>Depok</td>
+    </tr>
+    <tr>
+     <td>3</td>
+     <td>100122</td>
+     <td>Akihiro</td>
+     <td>Jakarta Pusat No 100</td>
+    </tr>
+    <tr>
+     <td>4</td>
+     <td>100123</td>
+     <td>Lia</td>
+     <td>Jakarta Selatan,Lebak Bulus</td>
+    </tr>
+   </table>
+  </div>
+ </div>
 </div>
-  
-  </body>
-   <script src="./js/jquery.min.js"></script>
-<script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+<script src="template/js/bootstrap.min.js"></script>
+</html>

9cbb65f425d80590560a4ed66c161a2cf8b1efdf
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-28 21:11:56 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="./css/bootstrap.min.css">
+ 
+    <title>Semangatt</title>
+  </head>
+  <body>
+  
+  
+   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="">♛ Toys Town</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+ 
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Dasboard<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Stock</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome  to Toys Town</h1>
+        <p class="lead">Set and manage your store inventory here.</p>
+        <hr class="my-4">
+        <p>Click the <strong>Stock</strong> Page to manage your shop inventory</p><br>
+         <a class="btn btn-danger btn-lg" href="#" role="button"> ← Logout</a>    
+        <a class="btn btn-primary btn-lg" href="#" role="button"> ♥ See about me</a>
+    </div>
+  
+  </body>
+   <script src="./js/jquery.min.js"></script>
+<script src="./js/bootstrap.bundle.min.js"></script>

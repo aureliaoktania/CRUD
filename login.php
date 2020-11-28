78edc6ee69cecc3969eb7af65a42b8db5535ff64
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-28 20:47:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,28 +3,26 @@
  session_start();
 
  if($_SERVER["REQUEST_METHOD"] == "POST") {
- // username and password sent from form
 
- $myusername = mysqli_real_escape_string($db,$_POST['username']);
- $mypassword = mysqli_real_escape_string($db,$_POST['password']);
- $sql = "SELECT id FROM admin WHERE username = '$myusername' and
-passcode = '$mypassword'";
+ $user = mysqli_real_escape_string($db,$_POST['username']);
+ $pass = mysqli_real_escape_string($db,$_POST['password']);
+ $sql = "SELECT id FROM user WHERE username = '$user' and pass = '$pass'";
  $result = mysqli_query($db,$sql);
  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
  $count = mysqli_num_rows($result);
 
- // If result matched $myusername and $mypassword, table row must be 1 row
  if($count == 1) {
- $_SESSION['login_user'] = $myusername;
+ $_SESSION['login_user'] = $user;
 
- header("location: welcome.php");
+ header("location: ../Dashboard.php");
  }else {
  echo "Your username or password is invalid";
 echo "<br />";
  }
  }
 ?>
+
 <html>
 
  <head>

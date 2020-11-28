5b75f9bd2d48f50d1d1d63da0d0695bffa0d393c
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-28 21:02:21 +0700

    Update login.php

--- login.php
+++ login.php
@@ -15,10 +15,27 @@
  if($count == 1) {
  $_SESSION['login_user'] = $user;
 
- header("location: ../Dashboard.php");
+echo ":D";
  }else {
  echo "Your username or password is invalid";
 echo "<br />";
  }
  }
 ?>
+
+<html>
+
+ <head>
+ <title>Login Page</title>
+
+ </head>
+
+ <form action ="" method = "post">
+ <label>UserName:</label><input type = "text" name = "username"
+value="your username" /><br /><br />
+ <label>Password:</label><input type = "password" name = "password" value
+="your pass" /><br/><br />
+ <input type = "submit" value = " Submit "/><br />
+ </form>
+ </body>
+</html>

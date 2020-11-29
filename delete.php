ebdf0a6b8fd274d897ff4c03109d4e09e10f4345
nandaari <58156683+nandaari@users.noreply.github.com>
2020-11-29 21:16:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,51 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "toystown";
-// Create connection
-$db = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$db) {
- die("Connection failed: " . mysqli_connect_error());
-}
-//Display records
-$sql = "SELECT ProductName, Code, Category, Price, StockCount FROM inventory";
-$result = mysqli_query($db, $sql);
-if (mysqli_num_rows($result) > 0) {
-// output data of each row
- while($row = mysqli_fetch_assoc($result)) {
-    echo "<td>" . $row["ProductName"]. "</td>" .
-    "<td>" . $row["Code"]."</td>". 
-    "<td>" . $row["Category"]. "</td>". 
-    "<td>" . $row["Price"]. "</td>". 
-    "<td>" . $row["StockCount"]. "</td>". "<br>";
-    }
-} else {
- echo "0 results";
-}
-echo "<br>We will delete record id 1 : <br>";
-// sql to delete a record
-$sql = "DELETE FROM inventory";
+include("config.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $prod_id       = $_POST['product_id'];
+
+$sql = "DELETE FROM inventory WHERE Code IN('$prod_id')";
+
 if (mysqli_query($db, $sql)) {
- echo "Record deleted successfully <br>";
+ echo "Delete Success.";
 } else {
- echo "Error deleting record: " . mysqli_error($conn);
-}
-$sql = "SELECT id, firstname, lastname FROM MembersTbl";
-$result = mysqli_query($db, $sql);
-//Display Records
-if (mysqli_num_rows($result) > 0) {
- // output data of each row
- while($row = mysqli_fetch_assoc($result)) {
-    echo "<td>" . $row["ProductName"]. "</td>" .
-    "<td>" . $row["Code"]."</td>". 
-    "<td>" . $row["Category"]. "</td>". 
-    "<td>" . $row["Price"]. "</td>". 
-    "<td>" . $row["StockCount"]. "</td>". "<br>";
- }
-} else {
- echo "0 results";
+ echo "Error: " . $sql . "<br>" . mysqli_error($db);
 }
 mysqli_close($db);
+
+header("location: menu.php");
+}
 ?>
